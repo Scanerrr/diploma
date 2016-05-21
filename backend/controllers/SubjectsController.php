b3c4f51a13a7a12da8c9a7a58dd0306fcb0587a6
Vadim(Wade) <wadedevelop@gmail.com>
2016-05-22 00:57:35 +0300

    fix

--- backend/controllers/SubjectsController.php
+++ backend/controllers/SubjectsController.php
@@ -77,7 +77,7 @@ class SubjectsController extends DefaultController
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect('index');
         } else {
             return $this->render('update', [
                 'model' => $model,
