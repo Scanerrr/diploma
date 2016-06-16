7443837d309904b1b54ff5009dbf8a514123effc
Vadim(Wade) <wadedevelop@gmail.com>
2016-06-17 00:39:27 +0300

    -first production ready commit

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,24 +1,9 @@
 <?php
-
-$config = [
+return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'eGFGcC7_-dfWVt1tovINM--l20JEmDcz',
+            'cookieValidationKey' => 'E1iMNQoSKEGMwsNIeK6YKaFDoA4kRKf0',
         ],
     ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;
